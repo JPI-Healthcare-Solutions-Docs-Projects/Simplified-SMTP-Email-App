5c74964de7048bbd2d953b07a848773aede57ca8
chitangchin <chitangb.chin@gmail.com>
2023-10-24 14:39:31 -0400

    Completed Successful email send

--- ExamVue Duo Email App/Form1.Designer.cs
+++ ExamVue Duo Email App/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.messageToSend = new System.Windows.Forms.RichTextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -38,30 +37,24 @@
             this.authentication = new System.Windows.Forms.LinkLabel();
             this.appPassword = new System.Windows.Forms.LinkLabel();
             this.gmailLabel = new System.Windows.Forms.Label();
-            this.messageLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.close = new System.Windows.Forms.Button();
+            this.send = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // messageToSend
-            // 
-            this.messageToSend.Location = new System.Drawing.Point(284, 285);
-            this.messageToSend.Name = "messageToSend";
-            this.messageToSend.Size = new System.Drawing.Size(239, 153);
-            this.messageToSend.TabIndex = 0;
-            this.messageToSend.Text = "";
-            this.messageToSend.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(284, 201);
+            this.textBox1.Location = new System.Drawing.Point(36, 194);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(181, 22);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(284, 126);
+            this.textBox2.Location = new System.Drawing.Point(34, 134);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(181, 22);
             this.textBox2.TabIndex = 2;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
@@ -72,15 +65,16 @@
             "gmail",
             "yahoo",
             "outlook"});
-            this.comboBox1.Location = new System.Drawing.Point(284, 54);
+            this.comboBox1.Location = new System.Drawing.Point(34, 73);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(94, 24);
             this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // emailProvider
             // 
             this.emailProvider.AutoSize = true;
-            this.emailProvider.Location = new System.Drawing.Point(281, 35);
+            this.emailProvider.Location = new System.Drawing.Point(33, 54);
             this.emailProvider.Name = "emailProvider";
             this.emailProvider.Size = new System.Drawing.Size(95, 16);
             this.emailProvider.TabIndex = 4;
@@ -89,7 +83,7 @@
             // emailAddress
             // 
             this.emailAddress.AutoSize = true;
-            this.emailAddress.Location = new System.Drawing.Point(281, 104);
+            this.emailAddress.Location = new System.Drawing.Point(31, 115);
             this.emailAddress.Name = "emailAddress";
             this.emailAddress.Size = new System.Drawing.Size(95, 16);
             this.emailAddress.TabIndex = 5;
@@ -98,7 +92,7 @@
             // password
             // 
             this.password.AutoSize = true;
-            this.password.Location = new System.Drawing.Point(281, 182);
+            this.password.Location = new System.Drawing.Point(33, 175);
             this.password.Name = "password";
             this.password.Size = new System.Drawing.Size(67, 16);
             this.password.TabIndex = 6;
@@ -108,47 +102,66 @@
             // authentication
             // 
             this.authentication.AutoSize = true;
-            this.authentication.Location = new System.Drawing.Point(554, 157);
+            this.authentication.Location = new System.Drawing.Point(182, 73);
             this.authentication.Name = "authentication";
             this.authentication.Size = new System.Drawing.Size(176, 16);
             this.authentication.TabIndex = 7;
             this.authentication.TabStop = true;
             this.authentication.Text = "Gmail 2 Step Authenthication";
+            this.authentication.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.authentication_LinkClicked);
             // 
             // appPassword
             // 
             this.appPassword.AutoSize = true;
-            this.appPassword.Location = new System.Drawing.Point(554, 182);
+            this.appPassword.Location = new System.Drawing.Point(182, 100);
             this.appPassword.Name = "appPassword";
             this.appPassword.Size = new System.Drawing.Size(217, 16);
             this.appPassword.TabIndex = 8;
             this.appPassword.TabStop = true;
             this.appPassword.Text = "Generate app password from gmail";
+            this.appPassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.appPassword_LinkClicked);
             // 
             // gmailLabel
             // 
             this.gmailLabel.AutoSize = true;
-            this.gmailLabel.Location = new System.Drawing.Point(554, 126);
+            this.gmailLabel.Location = new System.Drawing.Point(182, 44);
             this.gmailLabel.Name = "gmailLabel";
             this.gmailLabel.Size = new System.Drawing.Size(127, 16);
             this.gmailLabel.TabIndex = 9;
             this.gmailLabel.Text = "Mandatory for Gmail";
             // 
-            // messageLabel
+            // openFileDialog1
             // 
-            this.messageLabel.AutoSize = true;
-            this.messageLabel.Location = new System.Drawing.Point(284, 263);
-            this.messageLabel.Name = "messageLabel";
-            this.messageLabel.Size = new System.Drawing.Size(64, 16);
-            this.messageLabel.TabIndex = 10;
-            this.messageLabel.Text = "Message";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(36, 251);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(94, 29);
+            this.close.TabIndex = 12;
+            this.close.Text = "Close App";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(34, 222);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(94, 23);
+            this.send.TabIndex = 13;
+            this.send.Text = "Send Test";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.messageLabel);
+            this.ClientSize = new System.Drawing.Size(416, 302);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.close);
             this.Controls.Add(this.gmailLabel);
             this.Controls.Add(this.appPassword);
             this.Controls.Add(this.authentication);
@@ -158,7 +171,6 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.messageToSend);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -167,8 +179,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.RichTextBox messageToSend;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ComboBox comboBox1;
@@ -178,7 +188,9 @@
         private System.Windows.Forms.LinkLabel authentication;
         private System.Windows.Forms.LinkLabel appPassword;
         private System.Windows.Forms.Label gmailLabel;
-        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Button send;
     }
 }
 
--- ExamVue Duo Email App/Form1.cs
+++ ExamVue Duo Email App/Form1.cs
@@ -7,29 +7,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using MailKit;
+using MailKit.Security;
+using System.Runtime.Remoting.Messaging;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
 
 namespace ExamVue_Duo_Email_App
 {
     public partial class Form1 : Form
     {
+        string emailAddressText;
+        string emailAddressPassword;
+        string smtpServer;
+        int smtpPort;
+
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
+            emailAddressText = textBox2.Text;
         }
-
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            emailAddressPassword = textBox1.Text;
+        }
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    //Set up gmail smtp
+                    smtpServer = "smtp.gmail.com";
+                    smtpPort = 587;
+                    break;
+
+                case 1:
+                    //Set up yahoo smtp
+                    smtpServer = "smtp.mail.yahoo.com";
+                    smtpPort = 587;
+                    break;
+
+                case 2:
+                    //Set up outlook smtp
+                    smtpServer = "smtp-mail.outlook.com";
+                    smtpPort = 587;
+                    break;
+            }
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        private void Attachment_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string y = openFileDialog1.FileName;
+                Console.WriteLine(y);
+            }
+        }
+        //GMAIL 2FA and App Password Requirement
+        private void authentication_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://security.google.com/settings/security/signinoptions/two-step-verification");
+        }
+
+        private void appPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://security.google.com/settings/security/apppasswords");
+        }
+
+        //Close app
+        private void close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //SMTP Server request
+        private void send_Click(object sender, EventArgs e)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(emailAddressText));
+            email.To.Add(MailboxAddress.Parse("brandon.chin@jpihealthcare.com"));
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(TextFormat.Html) { Text = "<h1>Example HTML Message Body</h1>" };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(smtpServer, smtpPort, SecureSocketOptions.StartTlsWhenAvailable);
+                client.Authenticate(emailAddressText, emailAddressPassword);
+                client.Send(email);
+                client.Disconnect(true);
+            }
         }
     }
 }
